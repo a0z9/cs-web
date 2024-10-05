62b962a513b07eb39f663985cbe7aa670389daac
Andres <a0z9@rambler.ru>
2024-10-05 10:42:42 +0300

    students model

---
+++ WebApp-Day-01/WebApp-MVC-auth-cookiee/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApp_MVC_auth_cookiee.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WebApp-Day-01/WebApp-MVC-auth-cookiee/Models/StudentsModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebApp_MVC_auth_cookiee.Models
+{
+    public record class Student(string Id, String Pass, Role Role);
+    public record class Role(string Name);
+
+    public static class Students {
+
+        public static Role masterRole = new Role("master");
+        public static Role bachelorRole = new Role("bachelor");
+        public static Role phdRole = new Role("phd");
+
+
+        public static List<Student> students = new List<Student> { 
+        new Student(Id:"Basil",Pass:"111",Role:phdRole),
+        new Student(Id:"Peter",Pass:"111",Role:bachelorRole),
+        new Student(Id:"Andrei",Pass:"111",Role:bachelorRole),
+
+        };
+
+    }
+
+}
+    
