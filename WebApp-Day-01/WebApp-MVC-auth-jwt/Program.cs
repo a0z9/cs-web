96162135ba4230c94eac362d6613b666ce7128b3
Andres <a0z9@rambler.ru>
2024-10-07 05:26:54 +0300

    jwt init

--- WebApp-Day-01/WebApp-MVC-auth-jwt/Program.cs
+++ WebApp-Day-01/WebApp-MVC-auth-jwt/Program.cs
@@ -1,21 +1,47 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authentication;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(
-    option =>
-    {
-        option.LoginPath = "/home/login";
-        option.AccessDeniedPath = "/home/denied";
-    }
+//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+//    .AddCookie(
+//    option =>
+//    {
+//        option.LoginPath = "/home/login";
+//        option.AccessDeniedPath = "/home/denied";
+//    }
 
+//    );
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(
+    options => {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+          ValidateIssuer = true,
+          ValidateAudience = true,
+          ValidIssuer = AOPtions.ISSUER,
+          ValidAudience = AOPtions.AUDIENCE,
+          
+          ValidateLifetime = true,
+         
+          IssuerSigningKey = AOPtions.GetKey(),
+          ValidateIssuerSigningKey = true
+        };
+    }
+    
+    
     );
 
 builder.Services.AddAuthorization();
@@ -43,3 +69,15 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+public class AOPtions
+{
+    public const string ISSUER = "IKIT";
+    public const string AUDIENCE = "Student";
+    private const string key = "oqwieuoqwieu9127qwjlkajsnlkjlakjdlkjrqwljrlqkwjrlqwkrjlkjlkj";
+
+    public static SymmetricSecurityKey GetKey() {
+
+        return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+    }
+}
