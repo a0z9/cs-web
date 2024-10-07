6a2c444ea2b5fe2fd71e5ee94b959a7596d7b451
Andres <a0z9@rambler.ru>
2024-10-07 07:44:57 +0300

    JS FETCH token

--- WebApp-Day-01/WebApp-MVC-auth-jwt/wwwroot/js/site.js
+++ WebApp-Day-01/WebApp-MVC-auth-jwt/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+self.addEventListener('fetch', e => {
+    console.log("Fetch event");
+    console.log(e);
+
+
+
+});
