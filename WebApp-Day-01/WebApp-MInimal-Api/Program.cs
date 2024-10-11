9aca02bded121cdd3f9d25fed884598f2f076d3c
Andres <a0z9@rambler.ru>
2024-10-11 14:42:59 +0300

    simple api test - started db ibmemory test

--- WebApp-Day-01/WebApp-MInimal-Api/Program.cs
+++ WebApp-Day-01/WebApp-MInimal-Api/Program.cs
@@ -1,6 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using WebApp_MInimal_Api.Model;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<StudentDb>(opt =>
+
+{ opt.UseInMemoryDatabase("StudentsList");
+  opt.EnableDetailedErrors();
+}
+
+);
+
+//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/api", () => "Hello Api Test!");
 
 app.Run();
