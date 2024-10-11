357a844a8f4d6c23ebb679f9ba6e3c99a0b64918
Andres <a0z9@rambler.ru>
2024-10-11 14:33:31 +0300

    minimal api model

---
+++ WebApp-Day-01/WebApp-MInimal-Api/Model/Student.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApp_MInimal_Api.Model
+{
+    public class Student
+    {
+     public int Id { get; set; }
+     public string Name { get; set; }  
+     public bool IsReady { get; set; }
+    //....
+
+    }
+
+    public class StudentDb : DbContext
+    {
+        public StudentDb(DbContextOptions<StudentDb> opt
+            ) : base(opt)
+        { }
+
+        public DbSet<Student> Students => Set<Student>();
+
+    }
+}
